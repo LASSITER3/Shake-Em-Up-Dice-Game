fadf68e39f407a0641de0bd2bc579e117dbf250e
Carl Lassiter <CARL.LASSITER3@GMAIL.COM>
2017-01-16 05:48:06 -0600

    Add files via upload

---
+++ DiceImage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace ShakeEmUp
+{
+  public sealed partial class DiceImage : UserControl
+  {
+    private const int numOfFacesOnDice = 6;
+    public Image[] Faces = new Image[numOfFacesOnDice];
+
+    public DiceImage()
+    {
+      this.InitializeComponent();
+      createDiceFaceArray();
+    }
+    
+    private void createDiceFaceArray()
+    {
+      Faces[0] = Face1;
+      Faces[1] = Face2;
+      Faces[2] = Face3;
+      Faces[3] = Face4;
+      Faces[4] = Face5;
+      Faces[5] = Face6;
+    
+       }
+    public void DisplayDiceFace(int FaceID)
+    {
+      FaceID = FaceID - 1;
+      for (int i = 0; i < numOfFacesOnDice; i++)
+      {
+        if (i == FaceID)
+        {
+          Faces[i].Visibility = Visibility.Visible;
+        }
+        else
+        {
+          Faces[i].Visibility = Visibility.Collapsed;
+        }
+      }
+      }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace ShakeEmUp
+{
+  /// <summary>
+  /// An empty page that can be used on its own or navigated to within a Frame.
+  /// </summary>
+  public sealed partial class MainPage : Page
+  {
+
+    public int[] myDiceArray = new int[3];
+    public int Score = 0;
+    public Random RandomGenerator = new Random();
+
+    public MainPage()
+    {
+      this.InitializeComponent();
+      SetupGame();
+    }
+
+    private void SetupGame()
+    {
+      WinText.Text ="Click the \"Shake \'Em Up\" button to play the game";
+      ScoreText.Text = $"Your Score is: {Score}";
+    }
+
+    //event handler for button click to roll dice
+    private void btnRollDice_Click(object sender, RoutedEventArgs e)
+    {
+      // ValueofDiceText.Text = "Button Clicked";
+
+      for (int i = 0; i < myDiceArray.Length; i++)
+      {
+        int ValueOfDice = RandomGenerator.Next(1, 7);
+        myDiceArray[i] = ValueOfDice;
+      }
+
+      ValueofDiceText.Text = $"The value of your Dice roll are: {myDiceArray[0]}, {myDiceArray[1]}, and {myDiceArray[2]}.";
+
+      Dice1.DisplayDiceFace(myDiceArray[0]);
+      Dice2.DisplayDiceFace(myDiceArray[1]);
+      Dice3.DisplayDiceFace(myDiceArray[2]);
+
+      DiceGameLogic();
+    }
+
+
+    private void DiceGameLogic()
+    {
+      int newScore = 0;
+      string WinMessage = "";
+
+    
+      if (myDiceArray[0] == myDiceArray[1] && myDiceArray[0] == myDiceArray[2])
+      {
+        if (myDiceArray[0] < 6)
+        {
+          newScore = myDiceArray[0] * 100;
+          WinMessage = $"you rolled a {myDiceArray[0]} add {newScore} points!";
+        }
+        else
+        {
+          newScore = 1000;
+          WinMessage = "Tripple 6's... you just earned a 1000 points!";
+        }
+      }
+      else if (myDiceArray[0] == myDiceArray[1] || myDiceArray[0] == myDiceArray[2] || myDiceArray[1] == myDiceArray[2])
+      {
+        newScore = 50;
+        WinMessage = "You scored 50 points";
+      }
+      else
+      {
+        newScore = 0;
+        WinMessage = "Sorry.. No matches this roll";
+      }
+      Score = Score + newScore;
+      ScoreText.Text = $"Score: {Score}";
+      WinText.Text = WinMessage;
+
+    }
+  }
+}
